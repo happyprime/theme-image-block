aa66222073068237a6b6ae78d831d1c512564ae5
jeremyfelt <jeremy.felt@gmail.com>
2025-11-05 11:46:20 -0800

    Add support for registered theme image sizes

--- blocks/build/theme-image/index.asset.php
+++ blocks/build/theme-image/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7a2b70d857e40af2cf1b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f97d82e990dc108628c4');
