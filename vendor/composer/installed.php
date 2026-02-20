f3e7452266eeea1ce714f51e46450cb1fd95b969
jeremyfelt <jeremy.felt@gmail.com>
2026-02-20 10:04:44 -0800

    Bump composer hash

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'happyprime/theme-image-block',
         'pretty_version' => 'dev-trunk',
         'version' => 'dev-trunk',
-        'reference' => '531cc101b613bdc204e82d8c0f93797e63680158',
+        'reference' => '72ef305d8c4045013c8432801ea1d029cbd07cfc',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'happyprime/theme-image-block' => array(
             'pretty_version' => 'dev-trunk',
             'version' => 'dev-trunk',
-            'reference' => '531cc101b613bdc204e82d8c0f93797e63680158',
+            'reference' => '72ef305d8c4045013c8432801ea1d029cbd07cfc',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
